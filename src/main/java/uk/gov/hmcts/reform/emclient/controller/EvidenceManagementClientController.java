aacfa73f830d47da2152436288bbe83683bbf055
Ganesh Raja <ganeshraja.ramu@hmcts.net>
2018-04-20 15:27:06 +0100

    PR-01 (#20)
    * DIV-2118: Create CNP pipeline
    
    * DIV-2118: Create CNP pipeline
    
    * Fixed syntax error
    
    * Added a subscription variable
    
    * Added missing variables
    
    * Added "evidence_management_store_url" variable
    
    * Added "product" variable
    
    * Removed the product variable and changed the product name
    
    * changes to the product variable
    
    * Changed product name
    
    * DIV-2118: remove "Jenkinsfile"
    
    * DIV-2118: change some vars
    
    * DIV-2118: change some vars
    
    * DIV-2118: add sandbox env vars
    
    * DIV-2118: shorten evidence-management to em in CNP
    
    * DIV-2118: change health check url to /health
    
    * Added web.config
    
    Need to change the jar file to not have snapshot. This will be a change to the gradle file
    
    * changed the jar file name
    added smoke tests
    
    * DIV-2118: separate out vars that are different for different environments
    
    * DIV-2118: use CNP document store
    
    * DIV-2118: undo deletion of a var
    
    * temporarily move to tactical evidence management
    
    * temporarily move to tactical dm store only
    
    * Revert "temporarily move to tactical dm store only"
    
    This reverts commit 634b7ffa65cffa407cd382a2d82f4b75689ca438.
    
    * DIV-2308: Added aat & prod terraform var files
    
    * DIV-2308: Code cleanup to address PR comments
    
    * DIV-2308: Updated healthcheck urls to remove /status/
    
    * DIV-2308: Updated build.gradle to include functional test task
    
    * DIV-2308: Changed env base urls in terraform files
    
    * add saat.tfvars
    
    * em and cnp integration test changes
    
    * add insights
    
    * add saat.tfvars
    
    * Code changes to remove evidence mangement GW
    
    * Code changes to remove evidence mangement GW -build fix
    
    * added missing classes
    
    * amend AI Agent
    
    * added sonar exclusions
    
    * added missing terraform files
    
    * removed typo from build.gradle
    
    * add demo vars
    
    * add env values
    
    * changed micro service keys to ccd submission
    
    * added jenkins and params
    
    * modified em links with http
    
    * modified document store to http
    
    * take the most recent vars for aat and cnp
    
    * test fix
    
    * Fixed the urls
    
    * modified tests
    
    * fix func test
    
    * fix enbling log
    
    * added more debug
    
    * modified fix to integration test
    
    * Em client config changes
    
    * Fix Test Env Variable
    
    * use div-document-generator service credentials
    
    * Fix health check functional test
    
    * Changed the micro service to use div document generator's
    
    * Kill the smoke test
    
    * removed extra log
    
    * added fail after functional test code
    
    * Allow null auth key
    
    * push build to aat
    
    * make authtoken optional
    
    * Add unit test
    
    * added the code to fail after functional test AAT
    
    * add owasp dependency check suppressions
    
    * add preview.tfvars
    
    * add preview.tfvars
    
    * code clean up and unit test udate
    
    * remove classes dir for code review
    
    * Revert "add owasp dependency check suppressions"
    
    This reverts commit 84fe5ba68f8634acf7c9377ceeb00a5043ac70c0.
    
    * Owasp Fixes
    
    * Further gradle exclusions
    
    * code review issue fix
    
    * readd test exclusion
    
    * fix aat
    
    * Config changes
    
    * Config changes
    
    * code review
    
    * code review
    
    * Config changes and addtional logs
    
    * Config changes
    
    * Config changes
    
    * Config changes
    
    * Azure Vault key reading code
    
    * Azure Vault key reading code
    
    * Azure Vault key reading code
    
    * Azure Vault key reading code
    
    * Azure Vault key reading code
    
    * Azure Vault key reading code
    
    * Azure Vault key reading code
    
    * Azure Vault key reading code
    
    * Azure Vault key reading code
    
    * Infrastructure clean up
    
    * more clean up
    
    * more clean up
    
    * upgrade auth token library
    
    * put back the fail after aat functional test
    
    * changed the vault name
    
    * fail gate disable
    
    * Revert "fail gate disable"
    
    This reverts commit a3e1ee03b6a6ae9eb4335325f71edda3e550a244.

--- src/main/java/uk/gov/hmcts/reform/emclient/controller/EvidenceManagementClientController.java
+++ src/main/java/uk/gov/hmcts/reform/emclient/controller/EvidenceManagementClientController.java
@@ -1,8 +1,5 @@
 package uk.gov.hmcts.reform.emclient.controller;
 
-import java.io.IOException;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
@@ -16,11 +13,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import uk.gov.hmcts.reform.emclient.response.FileUploadResponse;
 import uk.gov.hmcts.reform.emclient.service.EvidenceManagementDownloadService;
 import uk.gov.hmcts.reform.emclient.service.EvidenceManagementUploadService;
 import uk.gov.hmcts.reform.emclient.validation.constraint.EvidenceFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/emclientapi")
@@ -33,32 +32,22 @@ public class EvidenceManagementClientController {
     @Autowired
     private EvidenceManagementDownloadService emDownloadService;
 
-    @PostMapping(value = "/version/1/uploadFilesWithS2SAuthToken", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+    @PostMapping(value = "/version/1/upload", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseBody
-    public List<FileUploadResponse> handleFileUploadWithS2SToken(
-            @RequestHeader(value = "authorizationToken") String authorizationToken,
+    public List<FileUploadResponse> upload(
+            @RequestHeader(value = "Authorization", required = false) String authorizationToken,
             @RequestHeader(value = "requestId", required = false) String requestId,
             @RequestParam("file") List<@EvidenceFile MultipartFile> files) {
 
-        return emUploadService.uploadFilesWithS2SAuthToken(files, authorizationToken, requestId);
+        return emUploadService.upload(files, authorizationToken, requestId);
     }
 
-    @PostMapping(value = "/version/1/uploadFiles", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
-            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @ResponseBody
-    public List<FileUploadResponse> handleFileUpload(
-            @RequestHeader(value = "authorizationToken") String authorizationToken,
-            @RequestHeader(value = "requestId", required = false) String requestId,
-            @RequestParam("file") List<@EvidenceFile MultipartFile> files) {
-
-        return emUploadService.uploadFilesWithUserAuthToken(files, authorizationToken, requestId);
-    }
 
     @GetMapping(value = "/version/1/downloadFile", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     @ResponseBody
     public ResponseEntity<InputStreamResource> downloadFile(
-            @RequestHeader(value = "authorizationToken") String authorizationToken,
+            @RequestHeader(value = "Authorization") String authorizationToken,
             @RequestHeader(value = "requestId", required = false) String requestId,
             @RequestParam("fileUrl") String selfFileUrl) throws IOException {
         return emDownloadService.downloadFile(selfFileUrl, authorizationToken, requestId);
---
+++ src/test/java/uk/gov/hmcts/reform/emclient/category/SmokeTest.java
@@ -0,0 +1,3 @@
+package uk.gov.hmcts.reform.emclient.category;
+
+public interface SmokeTest { /* category marker */ }
