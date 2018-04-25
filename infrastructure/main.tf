70a20f6371dfc421814a1651130db93de8dae9d2
Daniel Nenkov <daniel@avcora.com>
2018-04-25 15:30:46 +0100

    Trying to make preview env work:

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,14 +1,24 @@
 locals {
   ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
 
-  dm_store_url = "http://dm-store-${var.env}.service.${local.ase_name}.internal"
-  idam_s2s_url = "http://${var.idam_s2s_url_prefix}-${var.env}.service.${local.ase_name}.internal"
+  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+
+  dm_store_url = "http://dm-store-${local.local_env}.service.core-compute-${local.local_env}.internal"
+  idam_s2s_url = "http://${var.idam_s2s_url_prefix}-${local.local_env}.service.core-compute-${local.local_env}.internal"
+
+  previewVaultName = "${var.product}-${var.reform_service_name}"
+  nonPreviewVaultName = "${var.reform_team}-${var.reform_service_name}-${var.env}"
+  vaultName = "${var.env == "preview" ? local.previewVaultName : local.nonPreviewVaultName}"
+
+  nonPreviewVaultUri = "${module.key-vault.key_vault_uri}"
+  previewVaultUri = "https://div-${var.reform_service_name}-aat.vault.azure.net/"
+  vaultUri = "${var.env == "preview"? local.previewVaultUri : local.nonPreviewVaultUri}"
 }
 
 module "div-emca" {
   source       = "git@github.com:hmcts/moj-module-webapp.git?ref=master"
   product      = "${var.reform_team}-${var.reform_service_name}"
-  location     = "${var.location}"
+  location     = "${var.product}"
   env          = "${var.env}"
   ilbIp        = "${var.ilbIp}"
   subscription = "${var.subscription}"
@@ -36,7 +46,7 @@ module "div-emca" {
 # region save DB details to Azure Key Vault
 module "key-vault" {
   source              = "git@github.com:hmcts/moj-module-key-vault?ref=master"
-  name                = "${var.product}-${var.component}-${var.env}"
+  name                = "${local.vaultName}"
   product             = "${var.product}"
   env                 = "${var.env}"
   tenant_id           = "${var.tenant_id}"
