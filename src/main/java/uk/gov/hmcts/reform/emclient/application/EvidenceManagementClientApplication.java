78f4db3790499c691a07ea646591ed8bbdad25db
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-05-03 15:05:12 +0100

    disable second service auth healthcheck happening inside the service auth lib

--- src/main/java/uk/gov/hmcts/reform/emclient/application/EvidenceManagementClientApplication.java
+++ src/main/java/uk/gov/hmcts/reform/emclient/application/EvidenceManagementClientApplication.java
@@ -2,18 +2,11 @@ package uk.gov.hmcts.reform.emclient.application;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.HttpMessageConvertersAutoConfiguration;
-import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.feign.FeignAutoConfiguration;
-import org.springframework.cloud.netflix.feign.ribbon.FeignRibbonClientAutoConfiguration;
-import org.springframework.cloud.netflix.ribbon.RibbonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
