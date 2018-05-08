c0fba38c6ecf8119f61696f2d86115cb5dbe3b5e
David Gimelle <davidgimelle@gmail.com>
2018-05-08 11:24:36 +0100

    Improve coverage after jacoco test

---
+++ src/test/java/uk/gov/hmcts/reform/emclient/idam/UserServiceTest.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.emclient.idam;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.emclient.idam.api.IdamApiClient;
+import uk.gov.hmcts.reform.emclient.idam.models.UserDetails;
+import uk.gov.hmcts.reform.emclient.idam.services.UserService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+    @Mock
+    IdamApiClient idamApiClient;
+
+    @InjectMocks
+    UserService testObj;
+
+    @Test
+    public void userServiceReturnUserDetails(){
+
+        UserDetails userDetails = UserDetails.builder().id("IdOne").build();
+        String authToken = "authTokenValue";
+        when(idamApiClient.retrieveUserDetails(authToken)).thenReturn(userDetails);
+
+        assertThat(testObj.getUserDetails(authToken)).isEqualTo(userDetails);
+    }
+}
