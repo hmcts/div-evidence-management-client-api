5dc3c44265ab6a20e26a4a270b469556f31e263d
mthava <36265472+mthava@users.noreply.github.com>
2018-05-23 11:29:56 +0100

    Removed unused downloadFile endpoint (#47)
    * Removed unused downloadFile endpoint
    
    * Removed unnecessary full qualified name reference
    
    * send app insight to common location

--- src/main/java/uk/gov/hmcts/reform/emclient/controller/EvidenceManagementClientController.java
+++ src/main/java/uk/gov/hmcts/reform/emclient/controller/EvidenceManagementClientController.java
@@ -1,11 +1,8 @@
 package uk.gov.hmcts.reform.emclient.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import uk.gov.hmcts.reform.emclient.response.FileUploadResponse;
-import uk.gov.hmcts.reform.emclient.service.EvidenceManagementDownloadService;
 import uk.gov.hmcts.reform.emclient.service.EvidenceManagementUploadService;
 import uk.gov.hmcts.reform.emclient.validation.constraint.EvidenceFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -29,9 +24,6 @@ public class EvidenceManagementClientController {
     @Autowired
     private EvidenceManagementUploadService emUploadService;
 
-    @Autowired
-    private EvidenceManagementDownloadService emDownloadService;
-
     @PostMapping(value = "/version/1/upload", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseBody
@@ -41,15 +33,5 @@ public class EvidenceManagementClientController {
             @RequestParam("file") List<@EvidenceFile MultipartFile> files) {
 
         return emUploadService.upload(files, authorizationToken, requestId);
-    }
-
-
-    @GetMapping(value = "/version/1/downloadFile", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    @ResponseBody
-    public ResponseEntity<InputStreamResource> downloadFile(
-            @RequestHeader(value = "Authorization") String authorizationToken,
-            @RequestHeader(value = "requestId", required = false) String requestId,
-            @RequestParam("fileUrl") String selfFileUrl) throws IOException {
-        return emDownloadService.downloadFile(selfFileUrl, authorizationToken, requestId);
     }
 }
