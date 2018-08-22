79d153d76393c3c35643a3702533175c60b11cc9
David Gimelle <davidgimelle@gmail.com>
2018-08-22 15:22:51 +0100

    cnp vault _

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -27,7 +27,7 @@ module "div-emca" {
     REFORM_ENVIRONMENT                                    = "${var.env}"
     AUTH_PROVIDER_SERVICE_CLIENT_BASEURL                  = "${local.idam_s2s_url}"
     AUTH_PROVIDER_SERVICE_CLIENT_MICROSERVICE             = "${var.auth_provider_service_client_microservice}"
-    AUTH_PROVIDER_SERVICE_CLIENT_KEY                      = "${data.azurerm_key_vault_secret.div-doc-s2s-auth-secret.value}"
+    AUTH_PROVIDER_SERVICE_CLIENT_KEY                      = "${data.azurerm_key_vault_secret.div_doc_s2s_auth_secret.value}"
     AUTH_PROVIDER_SERVICE_CLIENT_TOKENTIMETOLIVEINSECONDS = "${var.auth_provider_service_client_tokentimetoliveinseconds}"
 
     DOCUMENT_MANAGEMENT_STORE_URL       = "${local.dm_store_url}"
@@ -55,16 +55,12 @@ module "key-vault" {
   product_group_object_id = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
 }
 
-provider "vault" {
-  address = "https://vault.reform.hmcts.net:6200"
-}
-
 data "azurerm_key_vault" "div_key_vault" {
   name                = "${local.vaultName}"
   resource_group_name = "${local.vaultName}"
 }
 
-data "azurerm_key_vault_secret" "div-doc-s2s-auth-secret" {
+data "azurerm_key_vault_secret" "div_doc_s2s_auth_secret" {
   name      = "div-doc-s2s-auth-secret"
   vault_uri = "${data.azurerm_key_vault.div_key_vault.vault_uri}"
 }
