62658cec94ef03569aa500fc7e5821dc91160b9b
Qiang Zhou <qiang.zhou@hmcts.net>
2019-03-20 12:38:22 +0000

    Fix test

--- src/test/java/uk/gov/hmcts/reform/emclient/service/EvidenceManagementDownloadServiceImplUTest.java
+++ src/test/java/uk/gov/hmcts/reform/emclient/service/EvidenceManagementDownloadServiceImplUTest.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.emclient.idam.models.UserDetails;
 import uk.gov.hmcts.reform.emclient.idam.services.UserService;
 
 import static org.junit.Assert.assertEquals;
@@ -29,6 +30,8 @@ public class EvidenceManagementDownloadServiceImplUTest {
     private static  final String TEST_AUTH_TOKEN = "testAuthToken";
     private static  final String TEST_REQUEST_ID = "testRequestId";
     private static  final String DM_STORE_URL = "http://dmstore.url";
+    private static  final String SERVICE_TOKE = "serviceToken";
+
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
@@ -52,6 +55,10 @@ public class EvidenceManagementDownloadServiceImplUTest {
     @Test
     public void givenDocumentId_whenDownloadFile_thenDMStoreServiceIsCalled(){
         ResponseEntity<byte[]> expectedResponse = mock(ResponseEntity.class);
+
+        when(userService.getUserDetails(TEST_AUTH_TOKEN)).thenReturn(UserDetails.builder().build());
+        when(authTokenGenerator.generate()).thenReturn(SERVICE_TOKE);
+
         String url = DM_STORE_URL+"/"+TEST_FILE_ID+"/binary";
         when(restTemplate.exchange(eq(url), eq(HttpMethod.GET), any(HttpEntity.class), eq(byte[].class)))
                 .thenReturn(expectedResponse);
