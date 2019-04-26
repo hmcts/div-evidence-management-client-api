efb5c7fbeabb79ac6bb3f08633469e709f3117dd
Tony Chow <36265570+tchow8@users.noreply.github.com>
2019-04-26 15:55:01 +0100

    Enable swagger

---
+++ src/main/java/uk/gov/hmcts/reform/emclient/config/SwaggerConfiguration.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.reform.emclient.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import uk.gov.hmcts.reform.emclient.application.EvidenceManagementClientApplication;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration implements WebMvcConfigurer {
+
+    @Value("${documentation.swagger.enabled}")
+    private boolean swaggerEnabled;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(EvidenceManagementClientApplication.class.getPackage().getName()))
+                .build()
+                .useDefaultResponseMessages(true)
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Divorce Evidence Management Client Api")
+                .description("Service to interact with the Reform Document Store")
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+        if (swaggerEnabled) {
+            registry.addResourceHandler("/swagger-ui.html**")
+                    .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+            registry.addResourceHandler("/webjars/**")
+                    .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+    }
+
+}
