56f074760c25980316ae2dceeee8be707a7cc108
Tony Chow <36265570+tchow8@users.noreply.github.com>
2019-06-07 13:16:28 +0100

    Set instance size to I3 (#103)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -26,6 +26,7 @@ module "div-emca" {
   common_tags                     = "${var.common_tags}"
   asp_name                        = "${local.asp_name}"
   asp_rg                          = "${local.asp_rg}"
+  instance_size                   = "I3"
 
   app_settings = {
     REFORM_SERVICE_NAME                                   = "${var.reform_service_name}"
