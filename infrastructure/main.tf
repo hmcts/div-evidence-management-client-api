f9ae11027a35ede2e4ab9016c44b7bea2d97a2ac
Patryk Jar <jar.patryk@gmail.com>
2019-06-27 17:45:10 +0100

    DIV-2579: https dm store urls for aat

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,14 +1,14 @@
 locals {
   aseName        = "core-compute-${var.env}"
   local_env      = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
-  dm_store_url   = "http://dm-store-${local.local_env}.service.core-compute-${local.local_env}.internal"
+  dm_store_url   = "https://dm-store-${local.local_env}.service.${local.local_env}.platform.hmcts.net"
   idam_s2s_url   = "http://${var.idam_s2s_url_prefix}-${local.local_env}.service.core-compute-${local.local_env}.internal"
 
   previewVaultName = "${var.reform_team}-aat"
   nonPreviewVaultName = "${var.reform_team}-${var.env}"
   vaultName = "${var.env == "preview" ? local.previewVaultName : local.nonPreviewVaultName}"
   vaultUri = "${data.azurerm_key_vault.div_key_vault.vault_uri}"
-  
+
   asp_name = "${var.env == "prod" ? "div-emca-prod" : "${var.raw_product}-${var.env}"}"
   asp_rg = "${var.env == "prod" ? "div-emca-prod" : "${var.raw_product}-${var.env}"}"
 }
