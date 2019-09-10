6bd1b5bedcf57250a2a7f5ca5f06d3eca57eb529
Tony Chow <36265570+tchow8@users.noreply.github.com>
2019-09-10 13:48:12 +0100

    Changed to use CNP Module Webapp

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,7 +14,7 @@ locals {
 }
 
 module "div-emca" {
-  source                          = "git@github.com:hmcts/moj-module-webapp.git?ref=master"
+  source                          = "git@github.com:hmcts/cnp-module-webapp?ref=master"
   product                         = "${var.product}-${var.reform_service_name}"
   location                        = "${var.location}"
   env                             = "${var.env}"
