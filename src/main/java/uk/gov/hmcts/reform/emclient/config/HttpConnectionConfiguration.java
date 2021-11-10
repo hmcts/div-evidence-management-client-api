c98acf52ad4e07588bd3d25849d6617843a4b242
Ruban <ruban72@hotmail.com>
2021-11-10 08:56:00 +0000

    DFR-395: Add Parameter Names Module to ObjectMapper config

--- src/main/java/uk/gov/hmcts/reform/emclient/config/HttpConnectionConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/emclient/config/HttpConnectionConfiguration.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.emclient.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import com.google.common.collect.ImmutableList;
 import org.apache.http.client.config.RequestConfig;
@@ -53,7 +54,9 @@ public class HttpConnectionConfiguration {
 
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         objectMapper.registerModule(new Jackson2HalModule());
+        objectMapper.registerModule(new JavaTimeModule());
         objectMapper.registerModule(new ParameterNamesModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
         MappingJackson2HttpMessageConverter jackson2HttpConverter
                 = new MappingJackson2HttpMessageConverter(objectMapper);
